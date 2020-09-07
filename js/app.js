aeb74eb5e6fcf89992ecb26d8aea032eed897bd0
couch Janus <couchjanus@gmail.com>
2020-09-07 15:20:01 +0300

    added unit 5

--- js/app.js
+++ js/app.js
@@ -5,22 +5,111 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 function addProductToCart(content, item) {
+    content.querySelector('.cart-item').setAttribute('id', item.getAttribute('data-id'));
     content.querySelector('.product-name').textContent = item.querySelector(".product-name").textContent;
     content.querySelector('.product-price').textContent = item.querySelector(".product-price").textContent;
+    content.querySelector('.product-price').setAttribute('data-price', item.querySelector(".product-price").textContent);
     content.querySelector('.product-img img').setAttribute('src',item.querySelector(".product-img").getAttribute('src'));
     return content;
     
 }
- 
+function createMarkup(data) {
+    return `
+    <div class="col-xl-3 col-lg-4 col-sm-6">
+           <div class="product text-center" data-id="${data.id}">
+               <div class="position-relative mb-3">
+                   <a class="d-block" href="detail.html">
+                       <img class="img-fluid w-100 product-img" src="${data.image}" alt="...">
+                       </a>
+                   <div class="product-overlay">
+                       <ul class="mb-0 list-inline">
+                           <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark"
+                                   href="#"><i class="far fa-heart"></i></a></li>
+                           <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark add-to-cart"
+                                   href="#">Add to cart</a></li>
+                           <li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark" href="#"><i
+                                       class="fas fa-expand"></i></a>
+                           </li>
+                       </ul>
+                   </div>
+               </div>
+               <h6> <a class="reset-anchor product-name" href="detail.html">${data.name}</a></h6>
+               <p class="small text-muted product-price" data-price="${data.price}">${data.price}</p>
+           </div>
+       </div>
+    `
+   } 
 // ==============================
 (function(){
     toggleBtn.addEventListener("click", function () {
         sidebar.classList.toggle("show-sidebar");
+        console.log(document.querySelectorAll('.cart-item').length);
+
+        let itemsInCart = document.querySelectorAll('.cart-item');
+
+        for (item of itemsInCart) {
+            // console.log(item);
+        
+            const price = item.querySelector('.product-price').getAttribute('data-price');
+
+            item.querySelector('button.dec-btn').addEventListener('click', function(e){
+                console.log(e.target.parentNode.nextElementSibling);
+                let val = e.target.parentNode.nextElementSibling.textContent;
+                if(+(val) > 1) {
+                    val--;
+                    console.log('val-- :', val);
+                    e.target.parentNode.nextElementSibling.textContent = val;
+                    e.target.closest('.quantity').nextElementSibling.querySelector('.product-price').textContent=val*price;
+                }
+            });
+
+            item.querySelector('button.inc-btn').addEventListener('click', function(e){
+                console.log(e.target.parentNode.previousElementSibling);
+                let val = e.target.parentNode.previousElementSibling.textContent;
+                // e.target.parentNode.previousElementSibling.value = +(val)+1;
+                +(val)++;
+                console.log('val++ :', val);
+                e.target.parentNode.previousElementSibling.textContent = val;
+                e.target.closest('.quantity').nextElementSibling.querySelector('.product-price').textContent=val*price;
+            });
+        };
+
+        // for(let i=0; i<itemsInCart.length;i++){
+        //     const price = itemsInCart[i].querySelector('.product-price').getAttribute('data-price');
+
+        //     itemsInCart[i].querySelector('.quantity button.dec-btn').addEventListener('click', function(e){
+        //         // console.log(e.target.parentNode.nextElementSibling);
+        //         let val = e.target.parentNode.nextElementSibling.textContent;
+        //         if(+(val) > 1) {
+        //             val--;
+        //             console.log('val-- :', val);
+        //             e.target.parentNode.nextElementSibling.textContent = val;
+        //             e.target.closest('.quantity').nextElementSibling.querySelector('.product-price').textContent=val*price;
+        //         }
+        //     });
+
+        //     itemsInCart[i].querySelector('.quantity button.inc-btn').addEventListener('click', function(e){
+        //         // console.log(e.target.parentNode.previousElementSibling);
+        //         let val = e.target.parentNode.previousElementSibling.textContent;
+        //         // e.target.parentNode.previousElementSibling.value = +(val)+1;
+        //         +(val)++;
+        //         console.log('val++ :', val);
+        //         e.target.parentNode.previousElementSibling.textContent = val;
+        //         e.target.closest('.quantity').nextElementSibling.querySelector('.product-price').textContent=val*price;
+        //     });
+        // }
     });
 
     closeBtn.addEventListener("click", function () {
         sidebar.classList.remove("show-sidebar");
     });
+
+    let result = '';
+    products.forEach(function(item) {
+        result+=createMarkup(item);
+    });
+    document.querySelector('.showcase').innerHTML = result;
+    
 
     // ********** close links ************
     const navToggle = document.querySelector(".nav-toggle");
@@ -40,17 +129,17 @@ function addProductToCart(content, item) {
     let content = document.getElementById("cartItem").content;  
     const addToCarts = document.getElementsByClassName("add-to-cart");
 
-    // for (let i=0; i<addToCarts.length; i++) {
-    //     addToCarts[i].addEventListener('click', function(){
-    //         document.querySelector('.cart-items').append(document.importNode(content, true));
-    //     })
-    // }
     const template = document.getElementById("cartItem").content;  
     for (let i=0; i<addToCarts.length; i++) {
         addToCarts[i].addEventListener('click', function(e){
             let item = e.target.closest('.product');
             let content = addProductToCart(template, item);
             document.querySelector('.cart-items').append(document.importNode(content, true));
+            
+            let q = document.querySelector('.cart-toggle small').textContent;
+            q++;
+            document.querySelector('.cart-toggle small').textContent=q;
+            
         })
     }
 })();
--- js/test.js
+++ js/test.js
@@ -1,139 +1,161 @@
 'use strict';
 
-// Метод createElement создает элемент html, тег которого передается в качестве параметра. 
+const product = {
+    id:1,
+    image:"images/product-1.jpg",
+    name:"Kui Ye Chen’s AirPods",
+    price:123
+};
 
-let col = document.createElement("div");
-const p = document.createElement("p");
-const product = document.createElement('div');
-let anchor = document.createElement("a");
-
-// createTextNode(text): создает и возвращает текстовый узел. 
-let anchorText = document.createTextNode("Joemalone Women prefume");
-// можем воспользоваться свойством textContent:
-p.textContent = "$25";
-
-// Метод appendChild добавляет элемент в конец списка дочерних элементов родителя. 
-// Если элемент уже существует он удаляется из текущего родителя и вставляется заново.
-//  <h6><a class="reset-anchor" href="detail.html">Joemalone Women prefume</a></h6>
-anchor.appendChild(anchorText);
-
-// <h6> <a class="reset-anchor" href="detail.html">Joemalone Women prefume</a></h6>
-anchor.href = "detail.html";
-product.className = "product";
-
-// 
-product.appendChild(p);
-// 
-col.appendChild(product);
-
-const row = document.querySelector('.row');
-row.appendChild(col);
-
-// Метод parent.insertBefore(узел, место вставки)
-// узел - Ссылка на вставляемый узел.
-// место вставки - Ссылка на элемент, перед которым необходимо вставить новый узел. Если аргумент равен null, то узел вставляется в конец родителя (то есть сработает, как appendChild()).
-// Данный метод позволяет вставить элемент в любое место, а не только в конец родителя.
-
-const h6 = document.createElement("h6");
-h6.appendChild(anchor);
-
-// parent
-let parent = document.querySelector('.product');
-// console.log(parent);
-
-// Вставка в самое начало родителя, то есть перед первым узлом
-parent.insertBefore(h6, parent.firstChild);
-
-const positionRelative = document.createElement("div");
-
-const productOverlay = document.createElement("div");
-
-const ul = document.createElement("ul");
-ul.className = "mb-0 list-inline";
-
-let li = document.createElement("li");
-li.className="list-inline-item m-0 p-0";
-
-let a = document.createElement("a");
-a.className = "btn btn-sm btn-dark add-to-cart";
-a.href = "#";
-a.textContent = "Add to cart";
-
-li.appendChild(a);
-// ul.appendChild(li);
-// Вставка в конец родителя, аналогично appendChild()
-ul.insertBefore(li, null);
-
-productOverlay.appendChild(ul);
-positionRelative.appendChild(productOverlay);
-
-parent.insertBefore(positionRelative, parent.firstChild);
-
-// Вставка перед конкретным элементом parentChild
-let parentChild = document.querySelector('.list-inline li');
-let liElement = document.createElement('li');
-liElement.innerHTML = '<a class="btn btn-sm btn-outline-dark" href="#"><i class="far fa-heart"></i> Like</a>';
-ul.insertBefore(liElement, parentChild);
-// Вставка после конкретного элемента parentChild
-ul.insertBefore(liElement, parentChild.nextSibling);
-
-// Добавить новый узел в документ можно простой вставкой HTML-кода в виде строки. Для этого используется метод insertAdjacentHTML().
-// parent.insertAdjacentHTML(место вставки, HTML-код)
-// место вставки - Позиция, куда необходимо вставить код. Это место указывается относительно самого элемента и может иметь одно из следующих значений:
-// beforebegin - непосредственно перед открывающим тегом.
-// afterbegin - сразу после открывающего тега.
-// beforeend - непосредственно перед закрывающим тегом.
-// afterend - сразу после закрывающего тега.
-
-ul.insertAdjacentHTML('beforeend', '<li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark" href="#"><i class="fas fa-expand"></i>View</a></li>');
+for (let key in product) { 
+    console.log(key + ': ' + product[key]);
+}
 
 
-// Метод replaceChild() удаляет один узел и вставляет на его место новый.
-// parent.replaceChild(новый узел, старый узел)
-// новый узел - Ссылка на вставляемый узел.
-// старый узел - Ссылка на удаляемый узел.
-
-
-let li1 = document.querySelector('li.list-inline-item'); // ссылка на 1 существующий элемент
-console.log(li1);
-
-let li2 = li1.nextSibling; // ссылка на 2 существующий элемент
-console.log(li2);
-
-// Метод возвращает ссылку на удаленный узел.
-let replaceLi = ul.replaceChild(li1, li2); // заменяем элемент li1 на li2
-
-ul.insertBefore(replaceLi, li1);
-
-// <script id="template-item" type="text/template">
-// </script>
-
-// Браузер содержимое тега <script> считает простым текстом, а так как в атрибуте type у него указан неизвестный ему MIME-тип, то интерпретировать или отображать он его не станет. 
-// Содержимое тега script можно получить после загрузки документа, обратившись к нему по id.
-
-
-let templateSource = document.getElementById("template-item").innerHTML;      
-
-const section = document.createElement("section");
-section.classList = "text-center border border-light p-5 my-3";
-
-section.innerHTML = templateSource;
-document.querySelector('.container').appendChild(section);
-
-
-let content = document.getElementById("cartItem").content;  
-// document.querySelector('.cart-items').appendChild(content);
-
-// Для использования template его необходимо активировать. 
-
-let addToCart = document.getElementById('addToCart');
-
-addToCart.addEventListener('click', function(){
-    document.querySelector('.cart-items').appendChild(content);
+products.forEach(function(item) {
+    console.log(item);
 });
 
-// Самый простой способ активации заключается в создании deep copy свойства .content с использованием метода  document.importNode().  Свойство .content является read-only.
+   
 
-// addToCart.addEventListener('click', function(){
-//     document.querySelector('.cart-items').append(document.importNode(content, true));
-// });
+// Чтобы создать шаблонную строку, необходимо использовать символ обратной кавычки (`):
+
+let newString = `A string`;
+
+// Шаблонные строки позволяют записывать значения переменных на нескольких строках. 
+// В отличие от обычных строк, в шаблонных строках можно использовать символы переноса строк:
+
+let listInline= `
+<ul class="mb-0 list-inline">
+    <li class="list-inline-item m-0 p-0">
+        <a class="btn btn-sm btn-outline-dark" href="#"><i class="far fa-heart"></i></a>
+    </li>
+    <li class="list-inline-item m-0 p-0">
+        <a class="btn btn-sm btn-dark add-to-cart" href="#">Add to cart</a>
+    </li>
+    <li class="list-inline-item mr-0">
+        <a class="btn btn-sm btn-outline-dark" href="#"><i class="fas fa-expand"></i></a>
+    </li>
+</ul>
+`;
+// Все пробельные символы в шаблонной строке, включая переносы строк и отступы, включаются «как есть» в результат.
+
+let productHtml = `
+<div class="col-xl-3 col-lg-4 col-sm-6">
+<div class="product text-center">
+    <div class="position-relative mb-3">
+
+        <a class="d-block" href="detail.html"><img class="img-fluid w-100 product-img"
+                src="images/product-1.jpg" alt="..."></a>
+        <div class="product-overlay">
+            <ul class="mb-0 list-inline">
+                <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark"
+                        href="#"><i class="far fa-heart"></i></a></li>
+                <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark add-to-cart"
+                        href="#">Add to cart</a></li>
+                <li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark" href="#"><i
+                            class="fas fa-expand"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <h6> <a class="reset-anchor product-name" href="detail.html">Kui Ye Chen’s AirPods</a></h6>
+    <p class="small text-muted product-price">$250</p>
+</div>
+</div>
+`;
+
+// Выражения: ${expression}. 
+
+// Синтаксис ${} позволяет вставить в скобки выражение, которое передаст свое значение. Можно использовать обычную строку.
+console.log(`${"Kui Ye Chen’s AirPods"}`);
+
+let name = "Kui Ye Chen’s AirPods";
+console.log(`${name}`);
+
+// В выражениях можно проводить любые математические операции.
+let price = 19.99;
+let q = 5;
+let prod = `The price of ${name} is ${price * q}`;
+
+// Выражения можно использовать и с более сложными объектами.
+
+let makeProduct = {
+    id:1,
+    image:"images/product-1.jpg",
+    name:"Kui Ye Chen’s AirPods",
+    price:123,
+    renderProduct() {
+        return `
+        <div class="col-xl-3 col-lg-4 col-sm-6">
+            <div class="product text-center">
+                <div class="position-relative mb-3">
+                    <a class="d-block" href="detail.html">
+                        <img class="img-fluid w-100 product-img" src="${this.image}" alt="...">
+                        </a>
+                    <div class="product-overlay">
+                        <ul class="mb-0 list-inline">
+                            <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark"
+                                    href="#"><i class="far fa-heart"></i></a></li>
+                            <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark add-to-cart"
+                                    href="#">Add to cart</a></li>
+                            <li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark" href="#"><i
+                                        class="fas fa-expand"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <h6> <a class="reset-anchor product-name" href="detail.html">${this.name}</a></h6>
+                <p class="small text-muted product-price">${this.price}</p>
+            </div>
+        </div>
+        `;
+    }
+};
+
+console.log(makeProduct.renderProduct());
+
+    // на основе шаблонных строк можно делать HTML шаблоны.
+    let data = {
+        id:1,
+        image:"images/product-1.jpg",
+        name:"Kui Ye Chen’s AirPods",
+        price:123
+    };
+    
+    // создадим разметку
+    function createMarkup(data) {
+     return `
+     <div class="col-xl-3 col-lg-4 col-sm-6">
+            <div class="product text-center">
+                <div class="position-relative mb-3">
+                    <a class="d-block" href="detail.html">
+                        <img class="img-fluid w-100 product-img" src="${data.image}" alt="...">
+                        </a>
+                    <div class="product-overlay">
+                        <ul class="mb-0 list-inline">
+                            <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark"
+                                    href="#"><i class="far fa-heart"></i></a></li>
+                            <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark add-to-cart"
+                                    href="#">Add to cart</a></li>
+                            <li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark" href="#"><i
+                                        class="fas fa-expand"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <h6> <a class="reset-anchor product-name" href="detail.html">${data.name}</a></h6>
+                <p class="small text-muted product-price">${data.price}</p>
+            </div>
+        </div>
+     `
+    }
+
+    document.querySelector('.row').innerHTML = createMarkup(data);
+    
+    let res = '';
+    products.forEach(function(item) {
+        res+=createMarkup(item);
+    });
+    document.querySelector('.row').innerHTML = res;
+    
